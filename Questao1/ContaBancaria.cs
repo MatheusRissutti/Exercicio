1496bfa87685bbb554a652b65dcaa7e5f19f4d8a
Matheus Rissutti <oito.de@hotmail.com>
2023-08-03 18:10:08 -0300

    Add project files.

---
+++ Questao1/ContaBancaria.cs
@@ -0,0 +1,35 @@
+﻿using System.Globalization;
+
+namespace Questao1
+{
+    class ContaBancaria
+    {
+        public ContaBancaria(int numero, string nomeTitular)
+        {
+            Numero = numero;
+            NomeTitular = nomeTitular;
+        }
+        public ContaBancaria(int numero, string nomeTitular, double depositoInicial)
+        {
+            Numero = numero;
+            NomeTitular = nomeTitular;
+            Saldo = depositoInicial;
+        }
+
+        private int Numero { get; }
+        public string NomeTitular { get; }
+        private double Saldo { get; set; }
+
+
+        public void Deposito(double valor)
+        {
+            if(valor > 0)
+                Saldo += valor;
+        }
+
+        public void Saque(double valor)
+        {
+
+        }
+    }
+}
