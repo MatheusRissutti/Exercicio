8a95e31fcc38c517245e40d75b74b5c803bccb81
Matheus Rissutti <oito.de@hotmail.com>
2023-08-03 18:36:28 -0300

    Questao1

--- Questao1/ContaBancaria.cs
+++ Questao1/ContaBancaria.cs
@@ -1,4 +1,6 @@
-﻿using System.Globalization;
+﻿using System;
+using System.Globalization;
+using Questao1;
 
 namespace Questao1
 {
@@ -16,20 +18,35 @@ namespace Questao1
             Saldo = depositoInicial;
         }
 
-        private int Numero { get; }
-        public string NomeTitular { get; }
-        private double Saldo { get; set; }
+        protected int Numero { get; }
+        protected string NomeTitular { get; set; }
+        protected double Saldo { get; set; }
 
+
+        public void AlteraNomeTitular(string nomeTitular)
+        {
+            NomeTitular = nomeTitular;
+        }
 
         public void Deposito(double valor)
         {
-            if(valor > 0)
+            if (valor > 0)
                 Saldo += valor;
+            else
+                Console.WriteLine("O valor de depósito deve ser positivo.");
         }
 
         public void Saque(double valor)
         {
+            if (valor > 0)
+                Saldo -= (valor + RegraNegocio.TaxaSaque);
+            else
+                Console.WriteLine("O valor de saque deve ser positivo.");
+        }
 
+        public string Detalhes()
+        {
+            return $"Conta {Numero}, Titular: {NomeTitular}, Saldo: $ {Saldo.ToString("0.00")}";
         }
     }
 }
---
+++ Questao1/RegraNegocio.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Questao1
+{
+    internal class RegraNegocio
+    {
+        public const double TaxaSaque = 3.50d;
+    }
+}
