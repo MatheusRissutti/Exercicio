1bb7fe79699b7f70841c42b3e199e0f2912f1280
Matheus Rissutti <oito.de@hotmail.com>
2023-08-04 01:48:39 -0300

    Questao 2

---
+++ Questao2/FootballMatch.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Questao2
+{
+    internal class JsonMockFootball
+    {
+        public int Page { get; set; }
+
+        public int Per_Page { get; set; }
+        public int Total { get; set; }
+        public int Total_Pages { get; set; }
+        public List<FootballMatch> Data { get; set; }
+    }
+
+    internal class FootballMatch
+    {
+        public string Competition { get; set; }
+        public int Year { get; set; }
+        public string Round { get; set; }
+        public string Team1 { get; set; }
+        public string Team2 { get; set; }
+        public string Team1Goals { get; set; }
+        public string Team2Goals { get; set; }
+    }
+}
--- Questao2/Program.cs
+++ Questao2/Program.cs
@@ -1,4 +1,6 @@
-﻿using Newtonsoft.Json;
+﻿using Questao2;
+using System.Net.Http.Json;
+using System.Web;
 
 public class Program
 {
@@ -6,25 +8,62 @@ public class Program
     {
         string teamName = "Paris Saint-Germain";
         int year = 2013;
-        int totalGoals = getTotalScoredGoals(teamName, year);
 
-        Console.WriteLine("Team "+ teamName +" scored "+ totalGoals.ToString() + " goals in "+ year);
-
-        teamName = "Chelsea";
-        year = 2014;
-        totalGoals = getTotalScoredGoals(teamName, year);
+        var totalGoals = GetTotalScoredGoals(teamName, year).GetAwaiter().GetResult();
 
         Console.WriteLine("Team " + teamName + " scored " + totalGoals.ToString() + " goals in " + year);
 
-        // Output expected:
-        // Team Paris Saint - Germain scored 109 goals in 2013
-        // Team Chelsea scored 92 goals in 2014
+        teamName = "Chelsea";
+        year = 2014;
+
+        totalGoals = GetTotalScoredGoals(teamName, year).GetAwaiter().GetResult();
+
+        Console.WriteLine("Team " + teamName + " scored " + totalGoals.ToString() + " goals in " + year);
     }
 
-    public static int getTotalScoredGoals(string team, int year)
+
+    public static async Task<int> GetTotalScoredGoals(string team, int year)
     {
-        
-        return 0;
+        var goals = await GetGoalsCount(year, team, "team1");
+
+        goals += await GetGoalsCount(year, team, "team2");
+
+        return goals;
     }
 
+    public static async Task<int> GetGoalsCount(int year, string team, string playSide)
+    {
+        int page = 1;
+        int goalsCount = 0;
+        var uriBuilder = new UriBuilder(Constants.JsonMockUrl);
+        var content = new JsonMockFootball();
+
+        using (var client = new HttpClient())
+        {
+            do
+            {
+                var url = HttpUtility.ParseQueryString(string.Empty);
+                url["year"] = year.ToString();
+                url[playSide] = team;
+                url["page"] = page.ToString();
+                uriBuilder.Query = url.ToString();
+                var request = new HttpRequestMessage(HttpMethod.Get, uriBuilder.ToString());
+                HttpResponseMessage response = await client.SendAsync(request);
+                content = await response.Content.ReadFromJsonAsync<JsonMockFootball>();
+
+                foreach (var partida in content.Data)
+                {
+                    if(playSide.Equals("team1"))
+                        goalsCount += int.Parse(partida.Team1Goals);
+                    else
+                        goalsCount += int.Parse(partida.Team2Goals);
+                }
+
+                page++;
+            }
+            while (page - 1 <= content.Total_Pages);
+        }
+
+        return goalsCount;
+    }
 }
